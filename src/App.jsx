940089874acda44529bb71d804ee90cf60eb492e
shalapy <salahshalapy999@gmail.com>
2025-02-15 23:25:22 +0200

    Pushed App component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25] overflow-hidden">
+        <Button className="mt-10">smething</Button>
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
